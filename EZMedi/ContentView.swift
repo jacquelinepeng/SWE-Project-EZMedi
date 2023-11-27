510ed776befde4d0914de8ca2a3c3a12cbc05380
Feiluan <89844330+Feiluan@users.noreply.github.com>
2023-11-27 11:53:08 +0400

    Initial Commit

---
+++ EZMedi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EZMedi
+//
+//  Created by Qinomi on 27/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EZMedi/EZMediApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZMediApp.swift
+//  EZMedi
+//
+//  Created by Qinomi on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct EZMediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
