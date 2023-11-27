ff8a61f24af4ff4906685895aedc193bb60bc5d2
jacquelinepeng <jp5982@nyu.edu>
2023-11-27 13:07:15 +0400

    Initial Commit

---
+++ EZMedi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EZMedi
+//
+//  Created by Jiangweilin Peng on 11/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EZMedi/EZMediApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZMediApp.swift
+//  EZMedi
+//
+//  Created by Jiangweilin Peng on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EZMediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
