5d393762fef2922fa3101cd678420a62ddeabce0
C-137-Jerry <zx1370@nyu.edu>
2023-11-27 08:00:58 -0800

    Initial Commit

---
+++ EZMedi/EZMediApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZMediApp.swift
+//  EZMedi
+//
+//  Created by dhzy on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct EZMediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
