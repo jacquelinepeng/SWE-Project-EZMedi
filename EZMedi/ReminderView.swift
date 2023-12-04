7ba7a885f5e0850beac28964a7e6e53ba1e2cb1c
jacquelinepeng <jp5982@nyu.edu>
2023-12-04 21:08:41 +0400

    reminder created

---
+++ EZMedi/ReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderView.swift
+//  EZMedi
+//
+//  Created by Jiangweilin Peng on 12/4/23.
+//
+
+import SwiftUI
+
+struct ReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderView()
+}
