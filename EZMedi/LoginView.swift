6c919c9fb759ae89f43bb7c465ad93a34e0f0aad
jacquelinepeng <jp5982@nyu.edu>
2023-12-05 02:35:07 +0400

    LogIn

--- EZMedi/LoginView.swift
+++ EZMedi/LoginView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 struct LoginPage: View {
-    @State private var username: String = ""
+    
+    let didCompleteLoginProcess: () -> ()
+
+
+    @State private var email: String = ""
     @State private var password: String = ""
-    var isSecurity = false
+    @State private var isLoggedIn = false
 
     var body: some View {
         NavigationView {
@@ -22,7 +28,7 @@ struct LoginPage: View {
                     .padding(.bottom, 50)
         
 
-                TextField("Username", text: $username)
+                TextField("Username", text: $email)
                     .padding()
                     .background(Color(.secondarySystemBackground))
                     .cornerRadius(5.0)
@@ -34,9 +40,10 @@ struct LoginPage: View {
                     .cornerRadius(5.0)
                     .padding(.bottom, 20)
 
-                Button(action: {
+                Button{
                     // Perform login action
-                }) {
+                    handleAction()
+                } label: {
                     Text("Login")
                         .font(.headline)
                         .foregroundColor(.white)
@@ -45,19 +52,24 @@ struct LoginPage: View {
                         .background(Color(hex:"2D9596"))
                         .cornerRadius(25.0)
                 }
-
+                
+                Text(self.LoginStatusMessage).foregroundColor(.red)
                 Spacer()
 
                 NavigationLink(destination: RegisterPage()) {
                     HStack{
                         Text("Don't have an account? ")
                             .foregroundColor(.black)
-                        Text("Sign up")
+                        Text("Register")
                             .foregroundColor(Color(hex:"2D9596"))
                     }
                 }
                 
                 Spacer()
+                
+                NavigationLink(destination: MainView(), isActive: $isLoggedIn) {
+                    EmptyView()
+                }
                 
             }
             .padding()
@@ -65,14 +77,37 @@ struct LoginPage: View {
             .ignoresSafeArea()
         }
     }
+    
+    private func handleAction(){
+        print("Login")
+        loginUser()
+    }
+    
+    @State var LoginStatusMessage = ""
+    
+    private func loginUser(){
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password){
+            result, err in
+            if let err = err {
+                print("Failed to login:", err)
+                self.LoginStatusMessage = "Failed to login: \(err)"
+                return
+            }
+            print("successfully login as user: \(result?.user.uid ?? "")")
+            self.LoginStatusMessage = "successfully login as user: \(result?.user.uid ?? "")"
+            isLoggedIn = false
+            
+            self.didCompleteLoginProcess()
+        }
+    }
 }
 
 
 struct RegisterPage: View {
     // Registration view content goes here
-    @State private var username: String = ""
+    @State private var email: String = ""
     @State private var password: String = ""
-    @State private var password_2: String = ""
+    @State private var username: String = ""
     
     var body: some View {
         VStack {
@@ -82,7 +117,7 @@ struct RegisterPage: View {
                 .foregroundColor(Color(hex:"2D9596"))
                 .padding(.bottom, 50)
             
-            TextField("Email address", text: $username)
+            TextField("Email address", text: $email)
                 .padding()
                 .background(Color(.secondarySystemBackground))
                 .cornerRadius(5.0)
@@ -94,14 +129,9 @@ struct RegisterPage: View {
                 .cornerRadius(5.0)
                 .padding(.bottom, 20)
             
-            SecureField("Enter password again", text: $password_2)
-                .padding()
-                .background(Color(.secondarySystemBackground))
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
             
             Button(action: {
-                // Perform login action
+                handleAction()
             }) {
                 Text("Sign up")
                     .font(.headline)
@@ -112,6 +142,8 @@ struct RegisterPage: View {
                     .cornerRadius(25.0)
             }
             
+            Text(self.LoginStatusMessage).foregroundColor(.red)
+            
             Spacer()
             Spacer()
             
@@ -120,10 +152,53 @@ struct RegisterPage: View {
         .background(Color(hex: "##E7EDEB"))
         .ignoresSafeArea()
     }
+    private func handleAction(){
+        print("register")
+        createNewAccount()
+    }
+    
+    @State var LoginStatusMessage = ""
+    
+    private func createNewAccount(){
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password){
+            result, err in
+            if let err = err{
+                print("Failed to create user!", err)
+                self.LoginStatusMessage = "Failed to create user!: \(err)"
+                return
+            }
+            
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            self.LoginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+            self.storeUserInformation()
+        }
+    }
+    
+    private func storeUserInformation(){
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else{
+            return
+        }
+        
+        print(uid,"this is uid")
+        
+        let userData = ["email": self.email, "uid": uid]
+        FirebaseManager.shared.firestore.collection("users")
+            .document(uid).setData(userData){
+                err in
+                if let err = err{
+                    print(err)
+                    self.LoginStatusMessage = "\(err)"
+                    return
+                }
+                print("You have Successfully save your account!")
+            }
+    }
 }
 
 struct LoginPage_Previews: PreviewProvider {
     static var previews: some View {
-        LoginPage()
+        LoginPage(didCompleteLoginProcess: {
+            
+        })
     }
 }
