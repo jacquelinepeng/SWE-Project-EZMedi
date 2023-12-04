a4c1c028195625bf685acc5ccebaf53dd7646ecc
jacquelinepeng <jp5982@nyu.edu>
2023-12-04 16:01:06 +0400

    log in

--- EZMedi/SplashScreenView.swift
+++ EZMedi/SplashScreenView.swift
@@ -14,7 +14,7 @@ struct SplashScreenView: View {
     
     var body: some View {
         if isActive{
-            MainView()
+            LoginPage()
         } else{
             VStack{
                 VStack{
