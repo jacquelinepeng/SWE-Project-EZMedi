7996dfee8fbf943a0053e086706735ae9cca0853
jacquelinepeng <jp5982@nyu.edu>
2023-12-04 16:01:06 +0400

    medicine detail view update

--- EZMedi/ProfileView.swift
+++ EZMedi/ProfileView.swift
@@ -8,29 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State var user: User
 
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("User Information")) {
-                    Text("Name: \(user.name)")
-                    Text("Email: \(user.email)")
-                }
-                
-                Section(header: Text("Medicine Library")) {
-                    ForEach(user.medicineLibrary, id: \.id) { medicine in
-                        Text(medicine.name)
-                    }
-                }
-            }
-            .navigationBarTitle("Profile", displayMode: .large)
-        }
+        Text("hi")
     }
-}
-
-struct User {
-    var name: String
-    var email: String
-    var medicineLibrary: [Medicine]
 }
