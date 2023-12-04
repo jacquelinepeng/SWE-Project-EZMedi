22e993703e2b99991213fbac34d7d3fc378123d6
jacquelinepeng <jp5982@nyu.edu>
2023-12-04 14:57:32 +0400

    search bar implementation

---
+++ EZMedi/Medicine.swift
@@ -0,0 +1,18 @@
+//
+//  Medicine.swift
+//  EZMedi
+//
+//  Created by Jiangweilin Peng on 12/4/23.
+//
+
+import SwiftUI
+
+struct Medicine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Medicine()
+}
